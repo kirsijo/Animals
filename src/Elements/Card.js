7f1e6b06b812a83dbb211880d5ea0f19bca2f490
Kirsi Jokela <kirsijo@gmail.com>
2022-04-05 16:07:07 +0300

    added search

--- src/Elements/Card.js
+++ src/Elements/Card.js
@@ -2,58 +2,35 @@ import React from "react";
 import classes from "./Card.module.css";
 
 const Card = (props) => {
-  console.log(props);
   return (
-    <div className={`${classes.card}`}>
-      <Image title={props.title} />
+    <div className={classes.card}>
+      <h3>{props.search}</h3>
+      <div>
+        <Image title={props.title} />
+        <button className={classes.remove} onClick={props.remove}>
+          {" "}
+          X
+        </button>
+      </div>
       <h2>{props.title}</h2>
-      <p>
-        <span className="material-icons">favorite</span>
-        {props.likes}
-      </p>
-      <button onClick={props.add}> Like </button>
+      <div className={classes.likes_container}>
+        <p>
+          <span className="material-icons">favorite</span>
+          {props.likes}
+        </p>
+        <button onClick={props.add}> Like </button>
+      </div>
     </div>
   );
 };
 
 const Image = (props) => {
   return (
-    <img src={`https://source.unsplash.com/1600x900/?${props.title}`}></img>
+    <img
+      src={`https://source.unsplash.com/1600x900/?${props.title}`}
+      alt={props.name}
+    ></img>
   );
 };
 
 export default Card;
-
-/*src={`https://source.unsplash.com/1600x900/?${props.title}`}*/
-
-/*
-const Button = (props) => {
-    return (
-      <button
-        className={`${classes.button} ${classes[props.type]}`}
-        onClick={props.click}
-      >
-        {props.value}
-        {props.children}
-      </button>
-    );
-  };
-  
-  export default Button;
-  */
-
-/*
-const Box = (props) => {
-  return (
-    <div className="box">
-      <h2>{props.name}</h2>
-      <p>{props.title}</p>
-      <p>{props.age}</p>
-      <ul>{props.lang}</ul>
-    </div>
-  );
-};
-
-export default Box;*/
-
-/* className={`${classes.button}*/
--- src/Elements/Main.js
+++ src/Elements/Main.js
@@ -1,23 +1,51 @@
 import React from "react";
 import Card from "./Card.js";
 import { animals } from "./animals.js";
+// js is not always needed as REACT assumes the file is JS
 import classes from "./Main.module.css";
 
+//State is needed to build up the rendering.
+
 class Main extends React.Component {
+  state = {
+    animals: animals,
+    isLoading: false,
+    search: "",
+  };
+
+  /*
   constructor(props) {
     super(props);
     this.state = { animals };
   }
+*/
+
+  searchHandler = (e) => {
+    this.setState({ search: e.target.value });
+  };
+
   render() {
+    const animalFilter = this.state.animals.filter((animal) => {
+      return animal.name
+        .toLowerCase()
+        .includes(this.state.search.toLowerCase());
+    });
+
     return (
       <main>
-        <div className={`${classes.animalsList}`}>
-          {this.state.animals.map((animal) => (
+        <div>
+          <h1>{this.state.animals.length} Animals </h1>
+          <div className={`${classes.animalsList}`}>
+            <input type="text" onChange={this.searchHandler} />
+          </div>
+          {animalFilter.map((animal) => (
             <Card
+              //search={this.state.search}
               key={animal.name}
               title={animal.name}
               likes={animal.likes}
               add={() => this.addLikeHandler(animal.name)}
+              remove={() => this.removeCardHandler(animal.name)}
             ></Card>
           ))}
         </div>
@@ -25,11 +53,23 @@ class Main extends React.Component {
     );
   }
 
+  removeCardHandler = (name) => {
+    const updatedArray = this.state.animals.filter(
+      (animal) => animal.name !== name
+    );
+    this.setState({
+      animals: updatedArray,
+    });
+  };
+
   addLikeHandler = (name) => {
     this.setState((state) => {
       const updatedanimalsList = state.animals.map((animal) => {
         if (animal.name === name) {
           return { ...animal, likes: animal.likes + 1 };
+          // to be able to edit the object, we need to open it with a spread method
+          // now we can select likes
+          //The Spread operator lets you expand an iterable like an object, string, or array into its elements while the Rest operator does the inverse by reducing a set of elements into one array.
         } else {
           return animal;
         }
